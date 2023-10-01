0721060c565257ba18292a682d1fd30d28205a82
Matthias <mroeckl@users.noreply.github.com>
2023-10-01 11:26:36 +0200

    Adds relative coloring to PricesPerMonthChart

--- src/components/ui/PricesPerMonthChart.js
+++ src/components/ui/PricesPerMonthChart.js
@@ -5,17 +5,29 @@ const PricesPerMonthChart = ({ data, months }) => {
   const avgPricesPerMonth = months.map((month) => {
     const priceDataOfThisMonth = data.filter((item) => new Date(item.dateTime).getMonth() == month);
     const sum = priceDataOfThisMonth.reduce((accumulator, curr) => accumulator + curr.price, 0);
-    return { month: month, averagePrice: sum / priceDataOfThisMonth.length };
+    return { month: month, avgPrice: sum / priceDataOfThisMonth.length };
   });
+
+  function calcRelativeValue(value) {
+    return (value - minAvgPrice) / (maxAvgPrice - minAvgPrice);
+  }
+
+  const maxAvgPrice = avgPricesPerMonth.reduce(function (max, v) {
+    return max.avgPrice > v.avgPrice ? max.avgPrice : v.avgPrice;
+  });
+
+  const minAvgPrice = avgPricesPerMonth.reduce(function (min, v) {
+    return min.avgPrice < v.avgPrice ? min.avgPrice : v.avgPrice;
+  });
+
   return (
     <div style={{ height: 400 }} data-testid="PricesPerMonthChart">
       <ResponsiveBar
         data={avgPricesPerMonth}
         indexBy="month"
-        keys={["averagePrice"]}
+        keys={["avgPrice"]}
         valueFormat={(value) => `${Number(value).toFixed(1)} ct/kWh`}
-        colorBy="indexValue"
-        colors={{ scheme: "blues" }}
+        colors={(d) => `hsl(${(1 - calcRelativeValue(d.value)) * 120}, 100%, 70%)`}
         axisLeft={{
           format: (value) => `${Number(value)} ct/kWh`,
         }}
--- src/components/ui/PricesPerMonthChart.spec.js
+++ src/components/ui/PricesPerMonthChart.spec.js
@@ -4,18 +4,24 @@ import PricesPerMonthChart from "./PricesPerMonthChart";
 import { resize } from "../../../tests/ResizeObserverTestSupport";
 
 const testData = [
-  { dateTime: "2023-01-01T01:00:00", price: 1 },
-  { dateTime: "2023-01-01T01:00:00", price: 2 },
-  { dateTime: "2023-02-01T01:00:00", price: 3 },
-  { dateTime: "2023-02-01T01:00:00", price: 4 },
+  { dateTime: "2023-01-01T02:00:00Z", price: 1 },
+  { dateTime: "2023-01-01T02:00:00Z", price: 2 },
+  { dateTime: "2023-02-01T02:00:00Z", price: 3 },
+  { dateTime: "2023-02-01T02:00:00Z", price: 4 },
+  { dateTime: "2023-03-01T02:00:00Z", price: 5 },
+  { dateTime: "2023-03-01T02:00:00Z", price: 6 },
 ];
 
 describe("PricesPerMonthChart", () => {
-  it("is rendered", async () => {
-    const { getByTestId } = render(<PricesPerMonthChart data={testData} months={[0, 1]} />);
+  it("renders proper labels", async () => {
+    const { getByTestId, getByText, queryByText } = render(<PricesPerMonthChart data={testData} months={[0, 1, 2]} />);
     act(() => {
       resize();
     });
     expect(getByTestId("PricesPerMonthChart"));
+    expect(getByText("2023-1")).toBeInTheDocument();
+    expect(getByText("2023-2")).toBeInTheDocument();
+    expect(getByText("2023-3")).toBeInTheDocument();
+    expect(queryByText("2023-4")).not.toBeInTheDocument();
   });
 });
