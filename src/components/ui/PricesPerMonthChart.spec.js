97904c3ff1c533b7774aae701f80b901c58a4036
Matthias <mroeckl@users.noreply.github.com>
2023-10-05 22:41:28 +0200

    Improves tests of PricesPerMonthChart

--- src/components/ui/PricesPerMonthChart.spec.js
+++ src/components/ui/PricesPerMonthChart.spec.js
@@ -4,12 +4,13 @@ import PricesPerMonthChart from "./PricesPerMonthChart";
 import { resize } from "../../../tests/ResizeObserverTestSupport";
 
 const testData = [
-  { dateTime: "2023-01-01T02:00:00Z", price: 3 },
-  { dateTime: "2023-01-01T02:00:00Z", price: 4 },
-  { dateTime: "2023-02-01T02:00:00Z", price: 1 },
-  { dateTime: "2023-02-01T02:00:00Z", price: 2 },
-  { dateTime: "2023-03-01T02:00:00Z", price: 5 },
-  { dateTime: "2023-03-01T02:00:00Z", price: 6 },
+  { dateTime: "2023-01-01T02:00:00Z", price: 3.1 },
+  { dateTime: "2023-01-02T02:00:00Z", price: 4.1 },
+  { dateTime: "2023-02-01T02:00:00Z", price: 1.2 },
+  { dateTime: "2023-02-02T02:00:00Z", price: 2.2 },
+  { dateTime: "2023-03-01T02:00:00Z", price: 5.3 },
+  { dateTime: "2023-03-02T02:00:00Z", price: 6.3 },
+  { dateTime: "2023-04-01T02:00:00Z", price: 7.4 },
 ];
 
 describe("PricesPerMonthChart", () => {
@@ -19,9 +20,12 @@ describe("PricesPerMonthChart", () => {
       resize();
     });
     expect(getByTestId("PricesPerMonthChart"));
-    expect(getByText("2023-1")).toBeInTheDocument();
-    expect(getByText("2023-2")).toBeInTheDocument();
-    expect(getByText("2023-3")).toBeInTheDocument();
+    expect(getByText("2023-1")).toBeVisible();
+    expect(getByText("2023-2")).toBeVisible();
+    expect(getByText("2023-3")).toBeVisible();
     expect(queryByText("2023-4")).not.toBeInTheDocument();
+    expect(getByText("1.7 ct/kWh")).toBeVisible();
+    expect(getByText("3.6 ct/kWh")).toBeVisible();
+    expect(getByText("5.8 ct/kWh")).toBeVisible();
   });
 });
