7427632103e0196011517f7478cac42488aeed4f
Matthias <mroeckl@users.noreply.github.com>
2023-11-05 15:49:34 +0100

    Improved code style

--- src/@lekoarts/gatsby-theme-minimal-blog/styles/code.ts
+++ src/@lekoarts/gatsby-theme-minimal-blog/styles/code.ts
@@ -2,18 +2,10 @@ import code from "@lekoarts/gatsby-theme-minimal-blog/src/styles/code";
 
 export default {
   ...code,
-  ".prism-code": {
-    fontSize: [1, 1, 1],
-    webkitOverflowScrolling: `touch`,
-    backgroundColor: `transparent`,
-    minWidth: `100%`,
-    mb: 0,
-    mt: 0,
-    overflow: `auto`,
-    '&[data-linenumber="false"]': {
-      ".token-line": {
-        pl: 3,
-      },
-    },
+  div: {
+    fontSize: `0.7em`,
+  },
+  'div[class~="gatsby-highlight"]': {
+    margin: `0`,
   },
 };
