0f7e70318cd73e5bf855899a4e8ac69e0355c1b3
Matthias <mroeckl@users.noreply.github.com>
2023-11-06 22:07:48 +0100

    Improves code styling

--- src/@lekoarts/gatsby-theme-minimal-blog/styles/code.ts
+++ src/@lekoarts/gatsby-theme-minimal-blog/styles/code.ts
@@ -2,10 +2,13 @@ import code from "@lekoarts/gatsby-theme-minimal-blog/src/styles/code";
 
 export default {
   ...code,
-  div: {
-    fontSize: `0.7em`,
-  },
   'div[class~="gatsby-highlight"]': {
     margin: `0`,
+  },
+  ".code-content": {
+    fontSize: `0.7em`,
+    padding: `2rem 1rem 1rem 1rem`,
+    color: `plain-color`,
+    backgroundColor: `plain-backgroundColor`,
   },
 };
