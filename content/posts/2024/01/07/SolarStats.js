d375bdba40b61d3469ae00735adca776ac787928
Matthias <mroeckl@users.noreply.github.com>
2024-01-07 15:19:23 +0100

    Adds post on registered PV

---
+++ content/posts/2024/01/07/SolarStats.js
@@ -0,0 +1,157 @@
+import React, { useState, useEffect } from "react";
+import { ResponsiveBar } from "@nivo/bar";
+import { containerClass, checkboxClass, locClass, captionClass } from "./SolarStats.module.css";
+
+const CORS_PROXY_URL = "https://corsproxy.io/?"; //"http://api.codetabs.com/v1/proxy?quest=";
+const MASTR_GEN_URL =
+  "https://www.marktstammdatenregister.de/MaStR/Einheit/EinheitJson/GetErweiterteOeffentlicheEinheitStromerzeugung";
+const MASTR_SUM_URL = "https://www.marktstammdatenregister.de/MaStR/Einheit/EinheitJson/GetSummenDerLeistungswerte";
+const INBETRIEBNAMEDATUM = "Betriebs-Status~eq~'35'~and~Inbetriebnahmedatum%20der%20Einheit";
+const SOLARFILTER = "Lage%20der%20Einheit~eq~'853,2961'";
+
+const keys = ["Registrierungen", "Wechselrichterleistung", "Modulleistung"];
+
+const Checkbox = ({ type = "checkbox", name, checked = false, onChange }) => {
+  return (
+    <label htmlFor={name} className={checkboxClass}>
+      <input type={type} name={name} id={name} checked={checked} onChange={onChange} />
+      {name}
+    </label>
+  );
+};
+
+const LocInput = ({ id, label, placeholder, type = "text", onChange }) => {
+  return (
+    <label htmlFor="gemeinde">
+      {label}:&nbsp;
+      <input id={id} className={locClass} placeholder={placeholder} type={type} onChange={onChange} />
+    </label>
+  );
+};
+
+const SolarStats = () => {
+  const [jsonData, setJsonData] = useState(null);
+  const [timer, setTimer] = useState(null);
+  const [gemeinde, setGemeinde] = useState("Deutschland");
+  const [enabledKeys, setEnabledKeys] = useState(["Registrierungen"]);
+
+  const getJsonData = async (gemeinde) => {
+    try {
+      const urls = getUrls(gemeinde);
+
+      const requests = urls.map((url) => fetch(url));
+      const responses = await Promise.all(requests);
+      const errors = responses.filter((response) => !response.ok);
+
+      if (errors.length > 0) {
+        throw errors.map((response) => Error(response.statusText));
+      }
+
+      const json = responses.map((response) => response.json());
+      const data = await Promise.all(json);
+
+      const prepData = new Array(4);
+
+      for (let i = 0; i <= 3; i++) {
+        prepData[i] = {};
+        prepData[i].year = i === 3 ? "heute" : "31.12." + (2021 + i);
+        prepData[i].Registrierungen = data[i * 2].Total;
+        prepData[i].Wechselrichterleistung = Math.round(data[i * 2 + 1].nettoleistungSumme);
+        prepData[i].Modulleistung = Math.round(data[i * 2 + 1].bruttoleistungSumme);
+      }
+
+      return prepData;
+    } catch (errors) {
+      console.error(errors);
+    }
+  };
+
+  const getUrls = (gemeinde) => {
+    const gemeindeFilter = gemeinde ? `Gemeinde~eq~'${gemeinde}'~and~` : "";
+    const urls = [];
+    for (let year = 2021; year <= 2024; year++) {
+      urls.push(
+        CORS_PROXY_URL +
+          encodeURIComponent(
+            `${MASTR_GEN_URL}?pageSize=0&filter=${gemeindeFilter}${SOLARFILTER}~and~${INBETRIEBNAMEDATUM}~lt~'31.12.${year}'`,
+          ),
+      );
+      urls.push(
+        CORS_PROXY_URL +
+          encodeURIComponent(
+            `${MASTR_SUM_URL}?gridName=extSEE&filter=${gemeindeFilter}${SOLARFILTER}~and~${INBETRIEBNAMEDATUM}~lt~'31.12.${year}'`,
+          ),
+      );
+    }
+    return urls;
+  };
+
+  useEffect(() => {
+    getJsonData().then((data) => {
+      setJsonData(data);
+    });
+  }, []);
+
+  const handleLocationChange = (event) => {
+    clearTimeout(timer);
+
+    const newTimer = setTimeout(() => {
+      getJsonData(event.target.value).then((data) => {
+        setJsonData(data);
+      });
+      setGemeinde(event.target.value ? event.target.value : "Deutschland");
+    }, 1000);
+
+    setTimer(newTimer);
+  };
+
+  const handleEnabledKeysChange = (event) => {
+    const newEnabledKeys = [...enabledKeys];
+    enabledKeys.indexOf(event.target.name) === -1
+      ? newEnabledKeys.push(event.target.name)
+      : newEnabledKeys.splice(enabledKeys.indexOf(event.target.name), 1);
+    setEnabledKeys(newEnabledKeys);
+  };
+
+  if (jsonData != null) {
+    return (
+      <div data-testid="BalconysolarStats" className={containerClass}>
+        <div className={captionClass}>
+          Registrierte PV-Anlagen auf Gebäuden in {gemeinde !== "" && <span>{gemeinde}</span>}
+        </div>
+        <LocInput id="gemeinde" label="Gemeinde" placeholder="Ganz Deutschland" onChange={handleLocationChange} />
+        {keys.map((item) => (
+          <Checkbox key={item} name={item} checked={enabledKeys.includes(item)} onChange={handleEnabledKeysChange} />
+        ))}
+        <ResponsiveBar
+          data={jsonData}
+          margin={{ top: 20, right: 50, bottom: 100, left: 60 }}
+          colors={{ scheme: "paired" }}
+          theme={{ fontSize: "14px" }}
+          label={(d) =>
+            `${d.value}` + (d.id === "Registrierungen" ? "" : " kW") + (d.id === "Modulleistung" ? "p" : "")
+          }
+          groupMode="grouped"
+          keys={enabledKeys}
+          indexBy="year"
+          legends={[
+            {
+              dataFrom: "keys",
+              anchor: "bottom",
+              direction: "row",
+              translateY: 60,
+              itemWidth: 100,
+              itemHeight: 20,
+              itemsSpacing: 80,
+              symbolSize: 16,
+              itemDirection: "left-to-right",
+            },
+          ]}
+        />
+      </div>
+    );
+  } else {
+    return <div>Leider ist ein Fehler beim Laden der Daten aufgetreten :-(</div>;
+  }
+};
+export default SolarStats;
